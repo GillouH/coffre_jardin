96da51d8850c68ba2c2a9693977c100a0fc4ce85
Gillou <gilles.hubert97@gmail.com>
2026-05-26 14:49:34 +0200

    Gestion des erreurs et ajout d'une 2ème version

--- index.php
+++ index.php
@@ -1,21 +1,54 @@
 <?php
 
-$db_hostname = $_SERVER["DB_HOSTNAME"];
-$db_login = $_SERVER["DB_LOGIN"];
-$db_password = $_SERVER["DB_PASSWORD"];
-$db_name = $_SERVER["DB_NAME"];
+try{
+    $mysql_client = new PDO(
+        dsn: implode(
+            separator: ";",
+            array: [
+                "mysql:host={$_SERVER["DB_HOSTNAME"]}",
+                "dbname={$_SERVER["DB_NAME"]}",
+                "charset=utf8",
+            ],
+        ),
+        username: $_SERVER["DB_LOGIN"],
+        password: $_SERVER["DB_PASSWORD"],
+        options: [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ],
+    );
+} catch(Exception $exception){
+    die("Erreur : {$exception->getMessage()}");
+}
 
-$pdo = new PDO(
-    dsn: "mysql:host=$db_hostname;dbname=$db_name",
-    username: $db_login,
-    password: $db_password,
-);
-$statement = $pdo->query(
-    query: "SELECT * FROM users",
-);
-$rows = $statement->fetchAll(
-    mode: PDO::FETCH_ASSOC,
-);
+$version = "simple";
+//$version = "préparée";
+
+if($version == "simple"){
+    $statement = $mysql_client->query(
+        query: "SELECT * FROM users",
+    );
+    $rows = $statement->fetchAll(
+        mode: PDO::FETCH_ASSOC,
+    );
+} elseif($version == "préparée"){
+    // "a BETWEEN b AND c" -> "b <= a <= c"
+    $select_all_query = "SELECT * FROM users WHERE id BETWEEN :id_min AND :id_max";
+    $recipes_statement = $mysql_client->prepare(
+        query: $select_all_query,
+    );
+    $recipes_statement->execute(
+        params: [
+            "id_min" => 0,
+            "id_max" => 100,
+        ],
+    );
+    $rows = $recipes_statement->fetchAll(
+        mode: PDO::FETCH_ASSOC,
+    );
+} else{
+    die("Version non reconnue : {$version}");
+}
+
 ?>
 
 <table>
@@ -29,7 +62,7 @@ $rows = $statement->fetchAll(
 foreach($rows as $row){
     echo "<tr>";
     foreach(["id", "login", "password"] as $field_name){
-        echo "<td>$row[$field_name]</td>";
+        echo "<td>{$row[$field_name]}</td>";
     }
     echo "</tr>";
 }
